d0873c1779a12a1d051aa398bf8c1b484faf2ff9
asfundmirza <asfundmirzaj@gmail.com>
2024-02-13 22:06:01 +0500

    done designing

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,31 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className=" text-white">Hello to zustand</h1>
+    <main className="flex min-h-screen flex-col items-center gap-5  p-24">
+      <h1 className=" text-white">Welcome to Zustand crud APP</h1>
+      <form>
+        <input
+          type="text"
+          className="flex p-3 w-full border-none "
+          placeholder="enter title"
+        />
+        <button className="flex items-center text-center p-2 px-5 bg-blue-300">
+          Add
+        </button>
+      </form>
+      <div className="flex flex-col w-[500px]  items-center justify-center">
+        <div className=" p-10 text-white  border-solid border-2 border-blue-300 ">
+          <h2>asfund</h2>
+          <div className="flex gap-2">
+            <button className="flex items-center text-center p-2  bg-blue-500">
+              Edit
+            </button>
+            <button className="flex items-center text-center p-2  bg-blue-500">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
