5e1cc52b8580c94a9c02061fabf427bf84ce4fb2
asfundmirza <asfundmirzaj@gmail.com>
2024-02-14 16:39:34 +0500

    added the diting functionality

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,11 @@ import { setPosts } from "@/titles/store";
 import { useState } from "react";
 
 export default function Home() {
-  const { posts, addPost, deletePost } = setPosts();
+  const { posts, addPost, deletePost, editPost } = setPosts();
   const [post, setPost] = useState("");
+  const [edit, setEdit] = useState(false);
+  const [editedPost, setEditedPost] = useState("");
+  const [editedPostIndex, setEditedPostIndex] = useState("");
 
   const handleAddPost = () => {
     const newPost = {
@@ -15,6 +18,19 @@ export default function Home() {
   };
   const handleDeletePost = (index) => {
     deletePost(index);
+  };
+  const handleEditPost = (post, index) => {
+    setPost(post.title);
+    setEdit(true);
+    setEditedPost(post);
+    setEditedPostIndex(index);
+  };
+  console.log(editedPost, editedPostIndex);
+  const editPostHandle = () => {
+    const update = { ...editedPost, title: post };
+    editPost(update, editedPostIndex);
+    setPost("");
+    setEdit(false);
   };
   return (
     <main className="flex min-h-screen flex-col items-center gap-5  p-24">
@@ -27,12 +43,21 @@ export default function Home() {
           value={post}
           onChange={(e) => setPost(e.target.value)}
         />
-        <button
-          onClick={(e) => handleAddPost(e.preventDefault())}
-          className="flex items-center text-center p-2 px-5 bg-blue-300"
-        >
-          Add
-        </button>
+        {edit ? (
+          <button
+            onClick={(e) => editPostHandle(e.preventDefault())}
+            className="flex items-center text-center p-2 px-5 bg-blue-300"
+          >
+            Edit
+          </button>
+        ) : (
+          <button
+            onClick={(e) => handleAddPost(e.preventDefault())}
+            className="flex items-center text-center p-2 px-5 bg-blue-300"
+          >
+            Add
+          </button>
+        )}
       </form>
       <div className="flex flex-col w-[500px]  items-center justify-center">
         {posts?.map((post, index) => (
@@ -42,8 +67,11 @@ export default function Home() {
           >
             <h2>{post?.title}</h2>
             <div className="flex gap-2">
-              <button className="flex items-center text-center p-2  bg-blue-500">
-                Add
+              <button
+                onClick={() => handleEditPost(post, index)}
+                className="flex items-center text-center p-2  bg-blue-500"
+              >
+                Edit
               </button>
               <button
                 onClick={() => handleDeletePost(index)}
--- src/titles/store.js
+++ src/titles/store.js
@@ -20,4 +20,15 @@ export const setPosts = create((set) => ({
       return { posts: state.posts.filter((_, i) => i !== index) };
     });
   },
+  editPost: (updatePost, idx) => {
+    set((state) => {
+      const updatedPost = state.posts.map((post, index) => {
+        if (index === idx) {
+          return { ...post, ...updatePost };
+        }
+        return post;
+      });
+      return { posts: updatedPost };
+    });
+  },
 }));
